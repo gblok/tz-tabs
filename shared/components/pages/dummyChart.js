99666244132cc7de0a30a412ccab22992014c5ea
gblok <gblok.web@gmail.com>
2017-12-12 01:32:45 +0200

    init

--- shared/components/pages/dummyChart.js
+++ shared/components/pages/dummyChart.js
@@ -1,2 +1,11 @@
 import {h} from '../../modules'
-export default props => h('main', [h('h1', 'Chart')])
+
+export default зкщзы => h('dl', {className: 'chart'}, [
+    h('dt', 'Browser market share June 2015'),
+    h('dd', {className: 'percentage percentage-11'}, [h('span', 'IE 11: 11.33%')]),
+    h('dd', {className: 'percentage percentage-49'}, [h('span', 'Chrome: 49.77%')]),
+    h('dd', {className: 'percentage percentage-16'}, [h('span', 'Firefox: 16.09%')]),
+    h('dd', {className: 'percentage percentage-5'}, [h('span', 'Safari: 5.41%')]),
+    h('dd', {className: 'percentage percentage-2'}, [h('span', 'Opera: 1.62%')]),
+    h('dd', {className: 'percentage percentage-2'}, [h('span', 'Android 4.4: 2%')])
+])
--- shared/components/pages/dummyTable.js
+++ shared/components/pages/dummyTable.js
@@ -1,7 +1,8 @@
 import {h} from '../../modules'
 
-export default ({tr = 10, td = 10}) => h('table', Array.from({length: tr}, (_, i) => h('tr', [
-        h('th', `th ${i}`),
-        ...Array.from({length: td}, (_, k) => h('td', `td  ${i}-${k}`))
-    ]))
-)
+export default ({tr = 10, td = 10}) =>
+    h('table', Array.from({length: tr}, (_, i) => h('tr', [
+            h('th', `th ${i}`),
+            ...Array.from({length: td}, (_, k) => h('td', `td  ${i}-${k}`))
+        ]))
+    )
